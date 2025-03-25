6780864058f481a848bdeb1534637e1d249ffef5
Grodelek <arczigrodel@wp.pl>
2025-03-25 14:05:05 +0100

    backend project created

---
+++ backend/course/src/main/java/com/project/course/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.project.course.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class HomeController {
+
+  @GetMapping("/")
+  public ResponseEntity<String> getMessage() {
+    return ResponseEntity.ok("Hello world!");
+  }
+}
