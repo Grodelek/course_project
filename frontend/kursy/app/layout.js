edd2c65f00e4aad8b22f8c17673b446af081dc4e
PosimAndrzej <posimandrzej@gmail.com>
2025-03-27 17:52:24 +0100

    Creating layout v.0.1

--- frontend/kursy/app/layout.js
+++ frontend/kursy/app/layout.js
@@ -1,15 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navigation from "./navigation";
+import logoImg from '@/assets/logo3.png';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -20,9 +12,29 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className=""
       >
-        {children}
+        <header>
+        <img src={logoImg.src} alt="Logo strony" width="100" height="100"/>
+        <Navigation />
+        </header>
+        <main>
+          <div className="m-10 rounded-xl bg-[#EEEEEE] min-h-9/10">
+            {children}
+          </div>
+        </main>
+        
+        <footer className="bg-black text-white">
+          <div>|</div>
+          <div>Michał Dańko</div>
+          <div>|</div>
+          <div>Artur Grodel</div>
+          <div>|</div>
+          <div>Filip Kłos</div>
+          <div>|</div>
+          <div>Andrzej Posim</div>
+          <div>|</div>
+        </footer>
       </body>
     </html>
   );
