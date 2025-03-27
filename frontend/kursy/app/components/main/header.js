0d23f326d9d1f9444e45a4b542cc6df2c5da8408
PosimAndrzej <posimandrzej@gmail.com>
2025-03-27 23:26:11 +0100

    Layout v.0.3

--- frontend/kursy/app/components/main/header.js
+++ frontend/kursy/app/components/main/header.js
@@ -1,14 +1,13 @@
 import logoImg from '@/assets/logo3.png';
-import Navigation from '../navigation/navigation';
+import Navigation from '../navigation/navigation.js';
 
 export default function Header() {
   return (
-        <header class="flex fixed top-0 w-1/1">
-
-            <div class="bg-white rounded-full ml-20">
+        <header className="flex fixed top-0 w-1/1">
+            <div className="bg-white rounded-full ml-20">
                 <img src={logoImg.src} alt="Logo strony" width="100" height="100"/>
             </div>
-            <div class="mr-20 flex w-1/1">
+            <div className="mr-20 flex w-1/1">
                 <Navigation />
             </div>
             <div>
