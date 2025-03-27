224d38d8c97b666e6796e784e8230e842f65fa46
Filip Kłos <157379115+RedDebil@users.noreply.github.com>
2025-03-27 22:46:26 +0100

    Test
    Test

---
+++ frontend/kursy/app/components/main/header.js
@@ -0,0 +1,19 @@
+import logoImg from '@/assets/logo3.png';
+import Navigation from '../navigation/navigation';
+
+export default function Header() {
+  return (
+        <header className="flex fixed top-0 w-1/1">
+
+            <div className="bg-white rounded-full ml-20">
+                <img src={logoImg.src} alt="Logo strony" width="100" height="100"/>
+            </div>
+            <div className="mr-20 flex w-1/1">
+                <Navigation />
+            </div>
+            <div>
+                
+            </div>
+        </header>
+  );
+}
