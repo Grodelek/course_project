f5f92dbe5322aa51cda056bb3496b427a8276be2
Grodelek <arczigrodel@wp.pl>
2025-03-29 15:54:43 +0100

    verification code mailer implemented

--- backend/course/src/main/java/com/project/course/controllers/UserController.java
+++ backend/course/src/main/java/com/project/course/controllers/UserController.java
@@ -1,12 +1,17 @@
 package com.project.course.controllers;
 
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.project.course.models.User;
+import com.project.course.models.UserVerificationDTO;
+import com.project.course.services.EmailSenderService;
 import com.project.course.services.UserService;
 
 @RestController
@@ -14,10 +19,13 @@ import com.project.course.services.UserService;
 public class UserController {
   private final UserService userService;
   private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
+  private final EmailSenderService emailSenderService;
 
   @Autowired
-  public UserController(UserService userService) {
+  public UserController(UserService userService,
+      EmailSenderService emailSenderService) {
     this.userService = userService;
+    this.emailSenderService = emailSenderService;
   }
 
   @PostMapping("/login")
@@ -25,11 +33,25 @@ public class UserController {
     return userService.verify(user);
   }
 
+  public static String generateCode() {
+    Random random = new Random();
+    StringBuilder code = new StringBuilder();
+    for (int i = 0; i < 6; i++) {
+      code.append(random.nextInt(10));
+    }
+    return code.toString();
+  }
+
   @PostMapping("/register")
   public User register(@RequestBody User user) {
     user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
     user.setRoles("USER");
     user.setIsConfirmed('0');
+    String verificationCode = generateCode();
+    UserVerificationDTO userVerificationDTO = new UserVerificationDTO(verificationCode);
+    emailSenderService.sendEmail(user.getEmail(),
+        "Spring User Account verification",
+        verificationCode);
     return userService.register(user);
   }
 }
