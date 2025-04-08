6512e18652a306a1d52efb19de79013c4a8b6a6f
Grodelek <arczigrodel@wp.pl>
2025-04-08 21:20:38 +0200

    cleaning

--- backend/course/src/main/java/com/project/course/controllers/UserController.java
+++ backend/course/src/main/java/com/project/course/controllers/UserController.java
@@ -10,24 +10,18 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.project.course.models.User;
 import com.project.course.models.UserDTO;
 import com.project.course.models.VerificationCodeDTO;
 import com.project.course.services.UserService;
-import com.project.course.services.UserVerificationCodeService;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
   private final UserService userService;
-  private final UserVerificationCodeService userVerificationCodeService;
 
   @Autowired
-  public UserController(
-      UserService userService,
-      UserVerificationCodeService userVerificationCodeService) {
+  public UserController(UserService userService) {
     this.userService = userService;
-    this.userVerificationCodeService = userVerificationCodeService;
   }
 
   @PostMapping("/login")
